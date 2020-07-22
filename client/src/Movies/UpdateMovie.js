b9c91f3a4c34a562ba638cb64b228abbcb63140e
shanon <13fritsh@gmail.com>
2020-07-22 17:29:26 -0500

    added the update form and handles about to return html elements

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -6,5 +6,57 @@ const initialMovie = {
     title: '',
     director: '',
     metascore: '',
-    stars: [],
-  };
+    stars: []
+};
+
+const UpdateForm = props =>{
+    const location = useLocation();
+    const params = useParams();
+    const { push } = useHistory();
+    const [movie, setMovie] = useState(initialMovie);
+
+    useEffect(() =>{
+        if (location.state) {
+            setMovie(location.state);
+        } else {
+            axios.get(`http://localhost:3000/api/movies/${params.id}`)
+                .then(res =>{
+                    console.log(res)
+                })
+                .catch(error =>{
+                    console.log(error)
+                });
+        };
+    }, []);
+
+    const handleChange = event =>{
+        event.persist();
+        setMovie({
+            ...movie,
+            [event.target.name]: event.target.value,
+            stars: [{
+                ...movie.stars,
+                [event.target.name]: event.target.value
+            }]
+        });
+    };
+
+    const handleSubmit = event =>{
+        event.preventDefault();
+        axios.put(`http://localhost:3000/api/movies/${movie.id}`, movie)
+            .then(res =>{
+                console.log(res)
+            })
+            .catch(error =>{
+                console.log(error)
+            });
+    };
+
+    return(
+        <div>
+            <h2>Update Movie:</h2>
+        </div>
+    );
+};
+
+export default UpdateForm;
